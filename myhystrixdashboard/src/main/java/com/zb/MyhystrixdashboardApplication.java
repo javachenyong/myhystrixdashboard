2e15f9be95bec91edf1578fca5b11e92b485cb91
nohao <531153139@qq.com>
2019-09-03 11:50:21 +0800

    myeureka

---
+++ myhystrixdashboard/src/main/java/com/zb/MyhystrixdashboardApplication.java
@@ -0,0 +1,15 @@
+package com.zb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class MyhystrixdashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyhystrixdashboardApplication.class, args);
+    }
+
+}
