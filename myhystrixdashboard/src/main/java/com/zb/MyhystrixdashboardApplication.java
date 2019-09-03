70f1425c4b6639613066db9077ee77f6e69b4066
nohao <531153139@qq.com>
2019-09-03 12:14:47 +0800

    update zhangsanfen

--- myhystrixdashboard/src/main/java/com/zb/MyhystrixdashboardApplication.java
+++ myhystrixdashboard/src/main/java/com/zb/MyhystrixdashboardApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 
 @SpringBootApplication
 @EnableHystrixDashboard
+//张三丰
 public class MyhystrixdashboardApplication {
 
     public static void main(String[] args) {
