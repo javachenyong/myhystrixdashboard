aa123c432df050bb54b53c8ae916be449d77f749
javachenyong <54828131+javachenyong@users.noreply.github.com>
2019-09-03 12:19:18 +0800

    Update MyhystrixdashboardApplication.java

--- myhystrixdashboard/src/main/java/com/zb/MyhystrixdashboardApplication.java
+++ myhystrixdashboard/src/main/java/com/zb/MyhystrixdashboardApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 @SpringBootApplication
 @EnableHystrixDashboard
 //张三丰
+//李小璐
 public class MyhystrixdashboardApplication {
 
     public static void main(String[] args) {
